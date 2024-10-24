ef7d2e69ff04a069f8978a76cbfc6f9adb3bd0e3
Kateryna <koliesnikova.kateryna@gmail.com>
2024-10-25 00:36:51 +0300

    done

--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,3 +1,41 @@
-export const CryptoHistory = () => {
-  return <h2>CryptoHistory</h2>;
+import { format } from 'date-fns';
+import style from './CryptoHistory.module.css';
+
+export const CryptoHistoryItem = ({ index, price, amount, date }) => {
+  return (
+    <>
+      <td className={style.td}>{index}</td>
+      <td className={style.td}>{price}</td>
+      <td className={style.td}>{amount}</td>
+      <td className={style.td}>{date}</td>
+    </>
+  );
+};
+
+export const CryptoHistory = ({ items }) => {
+  return (
+    <table className={style.table}>
+      <thead className={style.thead}>
+        <tr>
+          <th className={style.th}>№</th>
+          <th className={style.th}>PRICE</th>
+          <th className={style.th}>AMOUNT</th>
+          <th className={style.th}>DATE</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map((item, index) => (
+          <tr key={item.id} className={style.tr}>
+            <CryptoHistoryItem
+              index={index + 1}
+              price={item.price}
+              amount={item.amount}
+              date={format(new Date(item.date), 'MM/dd/yyyy, hh:mm a')}
+            />
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,3 +1,14 @@
-export const StatisticsItem = () => {
-  return <h3>StatisticsItem</h3>;
+import style from './StatisticsItem.module.css';
+import { IconContext } from 'react-icons';
+
+export const StatisticsItem = ({ title, total, icon: Icon }) => {
+  return (
+    <>
+      <IconContext.Provider value={{ size: '28px' }}>
+        <Icon />
+      </IconContext.Provider>
+      <span className={style.counter}>{total}</span>
+      <p className={style.text}>{title}</p>
+    </>
+  );
 };
